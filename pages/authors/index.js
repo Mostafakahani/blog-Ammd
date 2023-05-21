404cb74ea8ad489893a44d2d89053695a114bb01
Mostafa <kahanimostafa83@gmail.com>
2023-05-21 16:21:36 +0330

    Update authors Page

--- pages/authors/index.js
+++ pages/authors/index.js
@@ -1,25 +1,57 @@
 
+import Footer from '@/Commponent/Footer';
+import Header from '@/Commponent/Header';
 import fs from 'fs/promises'
 import path from 'path';
+import oneImg from './imgs/one.webp'
+import twoImg from './imgs/two.webp'
+import Image from 'next/image';
+import { useState } from 'react';
 const Authors = (props) => {
     const { authors } = props;
-    const data1 = authors.map((item) => {
+    const [itemAuthors, setItemAuthors] = useState(authors);
+    const data1 = itemAuthors.map((item) => {
         return (
-            <ul key={item.id}>
-                <li>
-                    {item.name}
+            <>
+                <div className='col-sm-4 d-flex justify-content-center flex-column'>
+                    <div className='w-60'>
+                        <Image className='w-100 h-100' src={twoImg} style={{ width: 'auto', borderRadius: '10px' }} />
 
-                </li>
-                    {item.about}
+                    </div>
+                    <div className='d-flex justify-content-center mb-5 flex-column'>
+                        <div className='col mt-3'>
+                            <p style={{fontWeight: 'bold'}}>{item.name}</p>
+                        </div>
+                        <div className='col'>
+                            <p>{item.about}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
 
-            </ul>
+
         )
     })
     return (
         <>
-            <div>
-                {data1}
+            <Header />
+            <div className='container mt-5'>
+                <div className='row'>
+                    <div className='d-flex justify-content-center flex-column'>
+                        <p className='text-dark' style={{fontWeight: 'bold', fontSize: '30px'}}>
+                            نویسندگان
+                        </p>
+                        <div className='row justify-content-center '>
+                            {data1}
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
+            <div>
+            </div>
+            <Footer />
         </>
     )
 

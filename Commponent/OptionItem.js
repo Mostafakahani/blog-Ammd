51a4a9d46e47d7eba810f47c6ed0d1802fc06122
Mostafa <kahanimostafa83@gmail.com>
2023-08-15 11:42:16 +0330

    desgin

--- Commponent/OptionItem.js
+++ Commponent/OptionItem.js
@@ -16,21 +16,82 @@ const StyledGridItem = styled(Grid)(({ theme }) => ({
     transitionDuration: '0.3s', // تغییر مدت زمان افکت ترانزیشن
 }));
 
+const itemDes = {
+    fontFamily: 'Allison, sans-serif !important',
+    textAlign: 'center',
+    variant: 'span',
+    fontSize: { md: 24, xs: 15 },
+    textAlign: 'center',
+    fontWeight: 600,
+    color: 'transparent',
+    display: 'flex',
+    justifyContent: 'center',
+    // mb: 3,
+    position: 'relative',
+    // top: '50%',
+    // left: '50%',
+    transform: 'translate(-50%, -50%)',
+    WebkitBackgroundClip: 'text',
+    backgroundImage: 'linear-gradient(to bottom, white, #171721)',
+    zIndex: 1,
+    userSelect: 'none',
+    MozUserSelect: 'none',
+    WebkitUserSelect: 'none',
+};
+
 const StyledImage = styled('img')({
-    maxWidth: '20%',
-    height: 'auto',
-    marginBottom: '10px',
-    marginLeft: '5px',
+    position: 'relative',
+
+
 });
 
 const OptionItem = () => {
     const OptionsList = [
-        { urlImg: 'https://abzarwp.com/static/uploads/2021/08/support1.png', text: 'لود شدن سایتت خیلی طول می کشه؟', caption: "این یک کپشن هست" },
-        { urlImg: 'https://abzarwp.com/static/uploads/2020/11/grids.png', text: 'لود شدن سایتت خیلی طول می کشه؟', caption: "این یک کپشن هست" },
-        { urlImg: 'https://abzarwp.com/static/uploads/2022/03/password.png', text: 'لود شدن سایتت خیلی طول می کشه؟', caption: "این یک کپشن هست" },
-        { urlImg: 'https://abzarwp.com/static/uploads/2022/01/advanced_field.png', text: 'لود شدن سایتت خیلی طول می کشه؟', caption: "این یک کپشن هست" },
+        { urlImg: '/icon.svg', text: '8 سال سابقه کار', caption: "8 years of work experience" },
+        { urlImg: '/icon.svg', text: 'برگزاری دوره ها', caption: "1300 successful students" },
+        { urlImg: '/icon.svg', text: ' فایل های رایگان', caption: "10 training courses" },
+        { urlImg: '/icon.svg', text: 'خرید تحت وب', caption: "years of work experience" },
     ];
-
+    const itemOne = {
+        ...itemDes,
+        top: '40px',
+        // left: '500px',
+        right: '-90px',
+        bottom: '0px',
+        marginTop: '-20px',
+        WebkitBackgroundClip: 'text',
+        backgroundImage: 'linear-gradient(to bottom, #fd346ee8 10%, #ffffff00 100%)',
+    };
+    const itemTwo = {
+        ...itemOne,
+        top: '40px',
+        // left: '500px',
+        right: '-70px',
+        bottom: '0px',
+        marginTop: '-20px',
+        WebkitBackgroundClip: 'text',
+        backgroundImage: 'linear-gradient(to bottom, #fd346ee8 10%, #ffffff00 100%)',
+    };
+    const itemThree = {
+        ...itemOne,
+        top: '40px',
+        // left: '500px',
+        right: '-80px',
+        bottom: '0px',
+        marginTop: '-20px',
+        WebkitBackgroundClip: 'text',
+        backgroundImage: 'linear-gradient(to bottom, #fd346ee8 10%, #ffffff00 100%)',
+    };
+    const itemFour = {
+        ...itemOne,
+        top: '40px',
+        // left: '500px',
+        right: '-87px',
+        bottom: '0px',
+        marginTop: '-20px',
+        WebkitBackgroundClip: 'text',
+        backgroundImage: 'linear-gradient(to bottom, #fd346ee8 10%, #ffffff00 100%)',
+    };
     return (
         <>
             {/* #5D00D0 */}
@@ -40,7 +101,8 @@ const OptionItem = () => {
                 justifyContent: 'center',
                 my: 3,
                 position: 'relative',
-                zIndex: '5'
+                zIndex: '5',
+
             }}>برای دیدن سوالات تصادفی جدیدتر می تونی صفحه رو رفرش کنی!</Typography>
 
             <Typography
@@ -60,7 +122,10 @@ const OptionItem = () => {
                     transform: 'translate(-50%, -50%)',
                     WebkitBackgroundClip: 'text',
                     backgroundImage: 'linear-gradient(to bottom, white, #171721)',
-                    zIndex: '1'
+                    zIndex: '1',
+                    userSelect: 'none',
+                    MozUserSelect: 'none',
+                    WebkitUserSelect: 'none'
                 }}
             >
                 Sample projects carried out in the course
@@ -74,29 +139,75 @@ const OptionItem = () => {
                     mb: 5,
                 }}
             >
-                <hr style={{ 
-                    color: 'red', width: '50%', textAlign: 'center', 
+                <hr style={{
+                    color: 'red', width: '50%', textAlign: 'center',
                     boxShadow: '#ff000087 0px 6px 20px 12px',
                     position: 'absolute',
+                    top: '400px',
+                }} />
+                <hr style={{
+                    color: 'red', width: '50%', textAlign: 'center',
+                    position: 'absolute',
                     top: '400px'
-                    
-                    
-                    }} />
+                }} />
             </Grid>
             <Grid container spacing={{ xs: 2, md: 3 }} sx={{ textAlign: 'center', justifyContent: 'center' }}>
-                {OptionsList.map((x, index) => (
-                    <Grid item xs={12} sm={6} md={3} key={index}>
-                        <StyledGridItem sx={{
-                            justifyContent: { xs: 'space-evenly', sm: 'space-evenly' }
-                        }}>
-                            <Box textAlign="center">
-                                <Typography fontSize={{ md: 12, xs: 11, sm: 11 }} overflow='hidden' fontWeight='600'>{x.text}</Typography>
-                                <Typography fontFamily='Yekan Bakh !important' fontSize={11}>{x.caption}</Typography>
-                            </Box>
-                            <StyledImage src={x.urlImg} />
-                        </StyledGridItem>
-                    </Grid>
-                ))}
+                {/* {OptionsList.map((x, index) => ( */}
+                <Grid item xs={12} sm={6} md={3}>
+                    <StyledGridItem sx={{
+                        justifyContent: { xs: 'space-evenly', sm: 'space-evenly' },
+                        flexDirection: 'column',
+                        backgroundColor: 'hsla(0,0%,100%,.03)',
+
+                    }}>
+                        <StyledImage src={'/icon.svg'} />
+                        <Box textAlign="center">
+                            <Typography sx={itemTwo}>8 years of work experience</Typography>
+                            <Typography textAlign='center' color='white' fontSize={{ md: 18, xs: 11, sm: 11 }} overflow='hidden' fontWeight='600'>8 سال سابقه کار</Typography>
+                        </Box>
+                    </StyledGridItem>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StyledGridItem sx={{
+                        justifyContent: { xs: 'space-evenly', sm: 'space-evenly' },
+                        flexDirection: 'column',
+                        backgroundColor: 'hsla(0,0%,100%,.03)'
+                    }}>
+                        <StyledImage src={'/icon.svg'} />
+                        <Box textAlign="center">
+                            <Typography sx={itemOne}>Moving Design with Photoshop</Typography>
+                            <Typography textAlign='center' color='white' fontSize={{ md: 18, xs: 11, sm: 11 }} overflow='hidden' fontWeight='600'>طراحی با فتوشاپ</Typography>
+                        </Box>
+                    </StyledGridItem>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StyledGridItem sx={{
+                        justifyContent: { xs: 'space-evenly', sm: 'space-evenly' },
+                        flexDirection: 'column',
+                        backgroundColor: 'hsla(0,0%,100%,.03)'
+                    }}>
+                        <StyledImage src={'/icon.svg'} />
+                        <Box textAlign="center">
+                            <Typography sx={itemThree}>1300 successful students</Typography>
+                            <Typography textAlign='center' color='white' fontSize={{ md: 18, xs: 11, sm: 11 }} overflow='hidden' fontWeight='600'>1300 دانشجوی موفق</Typography>
+                        </Box>
+                    </StyledGridItem>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StyledGridItem sx={{
+                        justifyContent: { xs: 'space-evenly', sm: 'space-evenly' },
+                        flexDirection: 'column',
+                        backgroundColor: 'hsla(0,0%,100%,.03)'
+                    }}>
+                        <StyledImage src={'/icon.svg'} />
+                        <Box textAlign="center">
+                            <Typography sx={itemFour}>10 training courses</Typography>
+                            <Typography textAlign='center' color='white' fontSize={{ md: 18, xs: 11, sm: 11 }} overflow='hidden' fontWeight='600'>برگزاری ده دوره آموزشی</Typography>
+                        </Box>
+                    </StyledGridItem>
+                </Grid>
+
+                {/* ))} */}
             </Grid>
             {/* </Container> */}
         </>

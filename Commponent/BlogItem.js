c6452674303d32b9018344dd36f7dfb5fdf7a32e
Mostafa <kahanimostafa83@gmail.com>
2023-08-20 11:17:57 +0330

    Blog Design

--- Commponent/BlogItem.js
+++ Commponent/BlogItem.js
@@ -1,7 +1,7 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import { Box, Grid, Typography, Button } from "@mui/material";
 
 const BlogItem = () => {
-
     const posts = [
         {
             id: 1,
@@ -16,30 +16,56 @@ const BlogItem = () => {
             img: '/02.webp'
         },
         {
-            id: 1,
+            id: 3,
             title: "نمونه پروپوزال طراحی سایت",
             content: "محتوای مطلب اول ...",
             img: '/03.webp'
         },
         {
-            id: 2,
+            id: 4,
+            title: "افکت اینستاگرام گزینه ی چپ یا راست",
+            content: "محتوای مطلب دوم ...",
+            img: '/04.webp'
+        },
+        {
+            id: 4,
+            title: "افکت اینستاگرام گزینه ی چپ یا راست",
+            content: "محتوای مطلب دوم ...",
+            img: '/04.webp'
+        },
+        {
+            id: 3,
+            title: "نمونه پروپوزال طراحی سایت",
+            content: "محتوای مطلب اول ...",
+            img: '/03.webp'
+        },
+        {
+            id: 4,
+            title: "افکت اینستاگرام گزینه ی چپ یا راست",
+            content: "محتوای مطلب دوم ...",
+            img: '/04.webp'
+        },
+        {
+            id: 4,
             title: "افکت اینستاگرام گزینه ی چپ یا راست",
             content: "محتوای مطلب دوم ...",
             img: '/04.webp'
         }
     ];
+    const handleGridClick = () => {
+        history.push(`/post/${'s'}`); // انتقال به مسیر مطلب با استفاده از history
+    };
 
     return (
-        <Grid container sx={{
-            maxWidth: '100%',
-
-        }}>
+        <Grid container >
             {posts.map((post) => (
                 <Grid
-                    
+                    onClick={handleGridClick}
+                    item
                     xs={12}
                     sm={6}
                     md={4}
+                    lg={3}
                     key={post.id}
                     sx={{
                         padding: '10px',
@@ -48,67 +74,43 @@ const BlogItem = () => {
                         alignItems: 'center',
                         backgroundColor: '#271f2a7d',
                         borderRadius: 2,
-                        margin: '5px',
-                        justifyContent: 'center',
+                        justifyContent: 'space-between',
+                        height: { md: '460px', xs: 'auto' },
+                        transition: 'box-shadow 0.3s, color 0.3s',
+                        border: "3px solid #0e0f11",
                         "&:hover": {
                             color: 'gray',
                             boxShadow: '0px 0px 6px rgba(0, 0, 0, 0.5)',
                         },
                     }}
                 >
-                    <Box
-                        component='img'
-                        src={post.img}
-                        alt={post.title}
-                        sx={{
-                            width: '100%',
-                            height: '100%',
-                            borderRadius: '10px',
-                        }}
-                    />
-                    <Box sx={{
-                        width: '100%',
-                        textAlign: 'right',
-                    }}>
-                        <Typography sx={{
-                            color: '#fff',
-                            fontWeight: 800,
-                            fontSize: '20px',
-                            mt: 2,
-                            lineHeight: '1.7em',
-                            textOverflow: 'ellipsis',
-                            fontWeight: 'normal',
-                            padding: '0 5px',
-                            overflowWrap: 'normal',
-                            margin: '10px 0px',
-                        }}>
+                    <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', flexWrap: 'nowrap' }}>
+                        <img
+                            src={post.img}
+                            alt={post.title}
+                            style={{
+                                width: '100%',
+                                height: 'auto',
+                                borderRadius: '10px',
+                            }}
+                        />
+                        <Typography variant="h6" sx={{ color: '#fff', fontWeight: 800, mt: 2, lineHeight: '1.7em', overflow: 'hidden', textOverflow: 'ellipsis' }}>
                             {post.title}
                         </Typography>
-                        <Box sx={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            my: 3,
-                        }}>
-                            <Typography sx={{ color: '#fff', mt: 1 }}>{post.content}</Typography>
-                            <Box
-                                component='button'
-                                sx={{
-                                    width: '100px',
-                                    height: '40px',
-                                    borderRadius: '10px',
-                                    backgroundColor: '#000',
-                                    color: '#fff',
-                                }}
-                            >
-                                Button
-                            </Box>
-                        </Box>
+                    </Box>
+                    <Box sx={{ width: '100%', textAlign: 'right', mt: 2 }}>
+                        <Typography sx={{ color: '#fff' }}>{post.content}</Typography>
+                        <Button
+                            variant="contained"
+                            sx={{ mt: 2, bgcolor: '#000', color: '#fff', borderRadius: '10px', width: '100%' }}
+                        >
+                            ادامه مطلب
+                        </Button>
                     </Box>
                 </Grid>
             ))}
         </Grid>
     );
 };
-
 
 export default BlogItem;
